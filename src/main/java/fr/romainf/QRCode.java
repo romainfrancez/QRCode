84c0202b646bcf7eb6ee496d6bd05a2fb46f99ad
Romain Francez <me@romainf.fr>
2014-10-16 12:37:58 +0700

    Changed output to SVG
    Removed output size as it doesn’t make sense with SVG

--- src/main/java/fr/romainf/QRCode.java
+++ src/main/java/fr/romainf/QRCode.java
@@ -29,21 +29,24 @@ import com.google.zxing.EncodeHintType;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
+import org.apache.batik.dom.svg.SVGDOMImplementation;
+import org.apache.batik.svggen.SVGGraphics2D;
 import org.apache.commons.cli.*;
+import org.w3c.dom.DOMImplementation;
+import org.w3c.dom.Document;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
+import java.awt.*;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.EnumMap;
 import java.util.Map;
 
 public class QRCode {
 
-    private static final String DEFAULT_IMAGE_FORMAT = "png";
-    private static final String DEFAULT_OUTPUT_FILE = "out.png";
-    private static final String DEFAULT_WIDTH = "300";
-    private static final String DEFAULT_HEIGHT = "300";
+    private static final String DEFAULT_OUTPUT_FILE = "out.svg";
+    private static final int DEFAULT_WIDTH = 0;
+    private static final int DEFAULT_HEIGHT = 0;
     private static final String DEFAULT_PIXEL_COLOUR = "0xFF000000";
     private static final int DEFAULT_MARGIN = 0;
 
@@ -74,15 +77,18 @@ public class QRCode {
             }
 
             String output = cmd.getOptionValue("o", DEFAULT_OUTPUT_FILE);
-            int width = Integer.parseInt(cmd.getOptionValue("w", DEFAULT_WIDTH));
-            int height = Integer.parseInt(cmd.getOptionValue("h", DEFAULT_HEIGHT));
             String pixelColourText = cmd.getOptionValue("c", DEFAULT_PIXEL_COLOUR);
             if (pixelColourText.startsWith("0x")) {
                 pixelColourText = pixelColourText.substring(2);
             }
-            int pixelColour = (int)Long.parseLong(pixelColourText, 16);
+            Color pixelColour;
+            if (pixelColourText.length() == 6) {
+                pixelColour = new Color(Integer.parseInt(pixelColourText, 16));
+            } else {
+                pixelColour = new Color((int) Long.parseLong(pixelColourText, 16), true);
+            }
 
-            writeQRCode(args[l - 1], width, height, output, pixelColour);
+            writeQRCode(args[l - 1], output, pixelColour);
 
         } catch (ParseException e) {
             System.out.println(e.getMessage());
@@ -100,24 +106,21 @@ public class QRCode {
 
     /**
      * Writes data as a QRCode in an image.
-     * TODO: too many arguments
      *
-     * @param data   String The data to encode in a QRCode
-     * @param width  int The width of the final image
-     * @param height int The height of the final image
-     * @param output String The output filename
-     * @param pixelColour int The colour of pixels representing bits
+     * @param data        String The data to encode in a QRCode
+     * @param output      String The output filename
+     * @param pixelColour Color The colour of pixels representing bits
      * @throws WriterException
      * @throws IOException
      */
-    private static void writeQRCode(String data, int width, int height, String output, int pixelColour) throws WriterException, IOException {
+    private static void writeQRCode(String data, String output, Color pixelColour) throws WriterException, IOException {
         Map<EncodeHintType, Object> hints = new EnumMap<EncodeHintType, Object>(EncodeHintType.class);
         hints.put(EncodeHintType.MARGIN, DEFAULT_MARGIN);
 
         BitMatrix bitMatrix = new QRCodeWriter().encode(data,
                 BarcodeFormat.QR_CODE,
-                width,
-                height,
+                DEFAULT_WIDTH,
+                DEFAULT_HEIGHT,
                 hints);
 
         File file = new File(output);
@@ -127,7 +130,9 @@ public class QRCode {
         if (!file.createNewFile() && !file.canWrite()) {
             throw new IOException("Cannot write file " + file);
         }
-        ImageIO.write(renderImage(bitMatrix, pixelColour), DEFAULT_IMAGE_FORMAT, file);
+
+        SVGGraphics2D graphics2D = renderSVG(bitMatrix, pixelColour);
+        graphics2D.stream(new FileWriter(file), true);
     }
 
     /**
@@ -142,22 +147,12 @@ public class QRCode {
                 .withDescription("file to write to")
                 .withArgName("FILENAME")
                 .create("o"));
-        options.addOption(OptionBuilder.withLongOpt("width")
-                .isRequired(false).hasArg(true)
-                .withDescription("image width")
-                .withArgName("WIDTH")
-                .create("w"));
-        options.addOption(OptionBuilder.withLongOpt("height")
-                .isRequired(false).hasArg(true)
-                .withDescription("image height")
-                .withArgName("HEIGHT")
-                .create("h"));
         options.addOption(OptionBuilder.withLongOpt("colour")
                 .isRequired(false).hasArg(true)
                 .withDescription("pixel colour")
                 .withArgName("COLOUR")
                 .create("c"));
-        options.addOption(null, "help", false, "print this message");
+        options.addOption("h", "help", false, "print this message");
 
         return options;
     }
@@ -173,24 +168,30 @@ public class QRCode {
     }
 
     /**
-     * Renders the QRCode data in an image.
+     * Renders the QRCode data in an SVG document.
      *
-     * @param matrix BitMatrix BitMatrix of the encoded QRCode
-     * @param pixelColour int The colour of pixels representing bits
-     * @return BufferedImage
+     * @param matrix      BitMatrix BitMatrix of the encoded QRCode
+     * @param pixelColour Color The colour of pixels representing bits
+     * @return SVGGraphics2D
      */
-    public static BufferedImage renderImage(BitMatrix matrix, int pixelColour) {
+    private static SVGGraphics2D renderSVG(BitMatrix matrix, Color pixelColour) {
+        DOMImplementation implementation = SVGDOMImplementation.getDOMImplementation();
+        String svgNS = SVGDOMImplementation.SVG_NAMESPACE_URI;
+        Document document = implementation.createDocument(svgNS, "svg", null);
+
+        SVGGraphics2D svgGenerator = new SVGGraphics2D(document);
+
         int width = matrix.getWidth();
         int height = matrix.getHeight();
 
-        BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-
         for (int x = 0; x < width; x += 1) {
             for (int y = 0; y < height; y += 1) {
-                bufferedImage.setRGB(x, y, matrix.get(x, y) ? pixelColour : 0x00FFFFF);
+                if (matrix.get(x, y)) {
+                    svgGenerator.setPaint(pixelColour);
+                    svgGenerator.fill(new Rectangle(x, y, 1, 1));
+                }
             }
         }
-
-        return bufferedImage;
+        return svgGenerator;
     }
 }
